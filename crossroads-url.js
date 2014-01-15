7278a2d1e130dbda30c58465ace750f46b9674eb
Nathaniel Higgins <nat@nath.is>
2014-01-15 13:22:13 +0000

    Add the actual code, remove empty license file.

---
+++ crossroads-url.js
@@ -0,0 +1,322 @@
+(function(root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['lodash', 'crossroads', 'eventEmitter'], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory(
+            require('lodash'),
+            require('crossroads'),
+            require('wolfy87-eventemitter')
+        );
+
+    } else {
+        root.Router = factory(root._, root.crossroads, root.EventEmitter);
+    }
+}(this, function(_, crossroads, EventEmitter) {
+
+    /**
+     * Crossroads-based URL router
+     *
+     * @param {string}     base The base url. Defaults to ''
+     * @param {Crossroads} cr   Pass in a custom crossroads instance
+     */
+    function Router(base, cr) {
+        // Bind all methods so we don't need to worry
+        // about context.
+        _.bindAll.apply(_, [this].concat(_.functions(this)));
+
+        // Create an instance of crossroads if one is not passed
+        cr = cr || crossroads.create();
+
+        // Assign cr to a property to use throughout.
+        // Is hidden.
+        // @see Router#getCrossroads
+        this._cr = cr;
+
+        // Assign the base as a property to use
+        this.base = base || '';
+
+        // If we're in the browser, use the default window object
+        // otherwise it'll be mocked, but that's for the tests to do
+        if (typeof window !== 'undefined') {
+            this.window = window;
+        }
+
+        // Communicate outwards somehow
+        this.messages = new EventEmitter();
+    }
+
+    _.extend(Router.prototype, {
+
+        /**
+         * Normalise config for controller methods. 
+         *
+         * #1
+         *
+         * @param {Object} config {
+         *     @param {string} controller Name of the controller
+         *     @param {string} action     Name of the action.
+         *                                Defaults to "init"
+         * }
+         *
+         * @param {string} action Name of the action. Defaults to "init"
+         *
+         * or
+         *
+         * #2
+         *
+         * @param {string} controller Name of the controller
+         * @param {action} action     Name of the action, defaults to "init"
+         */
+        normalise: function(config, action) {
+
+            // If we've been passed a controller instead of a config
+            // object, turn it into a config object.
+            if (typeof config === 'string') {
+                config = {
+                    controller: config
+                };
+            }
+
+            // Avoid mutating the passed object
+            config = _.extend({}, config);
+
+            // If we've been passed an action, add it to the config object
+            if (action) {
+                config.action = action;
+            }
+
+            // Add the default action to the config object
+            config.action = config.action || 'init';
+
+            return config;
+        },
+
+        /**
+         * Get the fragment from the window location
+         * @return {String}
+         */
+        getFragment: function() {
+            // Throw an error if we haven't mocked the window object
+            if (!this.window) {
+                throw new Error('Need to mock window for node');
+            }
+
+            return this.window.location.pathname.substr(this.base.length);
+        },
+
+        /**
+         * Return the passed instance of crossroads
+         * @return {Crossroads}
+         */
+        getCrossroads: function() {
+            return this._cr;
+        },
+
+        /**
+         * This function takes an array of functions and returns a function
+         * that calls them all with the correct context an arguments
+         * @param  {Array}    callbacks The array of functions to call
+         * @return {Function}           The function that will call them
+         */
+        callFunctions: function(callbacks) {
+            return function handleRouteClosure() {
+                var _args = arguments;
+                var _this = this;
+
+                _.each(callbacks, function(callback) {
+                    callback.apply(_this, _args);
+                });
+            }
+        },
+
+        /**
+         * This function handles a route.
+         * @param  {String}   url
+         * @param  {Function} callback
+         *
+         * @todo Explain this a bit more
+         * @todo Add test for this
+         */
+        handleRoute: function(url, callback) {
+
+            var messages = this.messages;
+
+            return function handleRouteClosure() {
+                var val = callback.apply(this, arguments);
+
+                messages.emit('route', url);
+
+                return val;
+            };
+        },
+
+        /**
+         * This routes a simple URL or set of URLs to a callback or set
+         * of callbacks.
+         * @param  {Array|string}   urls      A single url or an array of
+         *                                    URLs to route on
+         * @param  {Array|Function} callbacks A single function or an array
+         *                                    of functions to call for the
+         *                                    passed URLs
+         * @return {Router}                   this for chaining
+         */
+        on: function(urls, callbacks) {
+            // Get the instance of crossroads
+            var cr = this.getCrossroads();
+
+            // Make sure we've dealing with arrays regardless of the 
+            // passed arguments
+            urls      = toArray(urls);
+            callbacks = toArray(callbacks);
+
+            // Generate a callback which will call all of our callbacks
+            // as crossroads stops after the first match
+            var callFunctions = this.callFunctions(callbacks);
+
+            // Store reference to conrext
+            var router = this;
+
+            // Add each of our URls to crossroads
+            _.each(urls, function(url) {
+                cr.addRoute(url, router.handleRoute(url, callFunctions));
+            });
+
+            return this;
+        },
+
+        /**
+         * This assigns a set of fallback functions when no route is matched
+         * @param  {Array|Function} callbacks A single or set of functions
+         *                                    to be called when no routes are
+         *                                    matched
+         * @return {Router}                   this for chaining
+         */
+        fallback: function(callbacks) {
+            var cr = this.getCrossroads();
+
+            callbacks = toArray(callbacks);
+
+            _.each(callbacks, function(callback) {
+                cr.bypassed.add(callback);
+            });
+
+            return this;
+        },
+
+        /**
+         * This function generates a function that will handle a controller
+         * @param  {Object} config Config for the
+         *                         controller (controller, action)
+         * @return {Function}      Function that handles the controller
+         *
+         * @todo Look at making location of controllers more configurable
+         */
+        controllerHandler: function(config) {
+            // Store a safe reference to the router
+            var router = this;
+
+            return function controllerHandlerClosure() {
+                // Store our arguments and convert them to an array
+                // so that we can play with them
+                var args = _.toArray(arguments);
+
+                // Lets grab the controller from requirejs
+                router.window.require(
+                    ['controllers/' + config.controller],
+                    function(controller) {
+                        // Grab some variables from the router
+                        var oldFrag       = router.currentFragment;
+                        var oldController = router.currentController;
+                        var newFrag       = router.getFragment();
+
+                        // Add the oldFrag and newFrag to start of the array
+                        var args = _.merge([oldFrag, newFrag], args);
+
+                        // If we have an old controller, try to unload it
+                        if (oldController && oldController.unload) {
+                            oldController.unload.apply(oldController, args);
+                        }
+
+                        // Grab the method from the controller
+                        var method = controller[config.action];
+
+                        // If we have no method, throw an error
+                        if (!method) {
+                            throw new Error(
+                                'Cannot find action ' + config.action +
+                                ' on controller ' + config.controller
+                            );
+                        }
+
+                        var val = method.apply(controller, args);
+
+                        // Do some post call stuff
+                        router.messages.emit('controller', config);
+                        router.currentController = controller;
+                        router.currentFragment   = newFrag;
+
+                        return val;
+                    }
+                );
+            }
+        },
+
+        /**
+         * Assign a controller for a set of urls
+         *
+         * @see Router#normalise and Router#on for method signature
+         */
+        controller: function(urls, config, action) {
+            config = this.normalise(config, action);
+
+            this.on(urls, this.controllerHandler(config));
+
+            return this;       
+        },
+
+        /**
+         * Assign a fallback controller
+         *
+         * @see Router#normalise and Router#fallback for method signature
+         */
+        fallbackController: function(config, action) {
+            config = this.normalise(config, action);
+
+            this.fallback(this.controllerHandler(config));
+
+            return this;
+        },
+
+        /**
+         * Run the routes against a URL
+         * @param  {String} url URL to match again, default to
+         *                      Router#getFragment
+         */
+        run: function(url) {
+            url = url || this.getFragment();
+
+            this.getCrossroads().parse(url);
+
+            return this;
+        }
+    });
+
+    /**
+     * This function converts anything to an array by:
+     *
+     *  1. if it's a primitive, wrap it an array with the passed item
+     *     as the only item.
+     *  2. If it's not a primitive, delegate to _.toArray
+     */
+    function toArray(item) {
+
+        if (typeof item === 'object') {
+            return _.toArray(item);
+        }
+
+        return [item];
+    };
+
+    return Router;
+
+}));
---
+++ test/test.js
@@ -0,0 +1,595 @@
+var assert     = require('assert');
+var Router     = require('../crossroads-url');
+var crossroads = require('crossroads');
+var Window     = require('window-location');
+
+var testUrl =
+	'http://localhost:8080/things/0/stuff/0?whatsits=1#majiggers';
+
+describe('Router', function() {
+
+	function getRouter() {
+		return new Router();
+	}
+
+	/**
+	 * Mock for requirejs
+	 *
+	 * Should probably improve this
+	 */
+	function getRequire(controllers) {
+		return function require(modules, cb) {
+			var name       = modules[0];
+			var controller = name.substr('controllers/'.length);
+			var module     = controllers[controller];
+
+			setTimeout(function() {
+				cb(module);
+			});
+		}
+	}
+
+	function getRouterWithRequire(controllers) {
+		var router = new Router();
+		var window = new Window();
+
+		window.require = getRequire(controllers);
+
+		router.window = window;
+		router.window.location = testUrl;
+
+		return router;
+	}
+
+	describe('#normalise()', function() {
+		it('should not mutate objects', function() {
+			var obj  = {};
+			var json = JSON.stringify(obj);
+
+			getRouter().normalise(obj);
+
+			assert.equal(json, JSON.stringify(obj));
+		});
+
+		it(
+			'should turn a controller an action into correct object',
+			function() {
+				assert.equal(JSON.stringify({
+					controller : 'Foo',
+					action     : 'bar'
+				}), JSON.stringify(getRouter().normalise('Foo', 'bar')));
+			}
+		);
+
+		it('should turn a controller into correct object', function() {
+			assert.equal(JSON.stringify({
+				controller : 'Foo',
+				action     : 'init'
+			}), JSON.stringify(
+				getRouter().normalise('Foo')
+			));
+		});
+
+		it('should add an action to config object', function() {
+			assert.equal(JSON.stringify({
+				controller : 'Foo',
+				action     : 'init'
+			}), JSON.stringify(
+				getRouter().normalise({
+					controller: 'Foo'
+				})
+			));
+		});
+
+		it('should do nothing to a valid config object', function() {
+			assert.equal(JSON.stringify({
+				controller : 'Foo',
+				action     : 'bar'
+			}), JSON.stringify(
+				getRouter().normalise({
+					controller : 'Foo',
+					action     : 'bar'
+				})
+			));
+		});
+	});
+
+	describe('#getCrossroads()', function() {
+
+		it('should return the passed instance of crossroads', function() {
+			var cr     = crossroads.create();
+			var router = new Router(undefined, cr);
+
+			assert.equal(cr, router.getCrossroads());
+		});
+	});
+
+	describe('#getFragment()', function() {
+		function getWindow() {
+			var window = new Window();
+
+			// Magic going on under the hood here
+			window.location = testUrl;
+
+			return window;
+		}
+
+		it('should error if we have not mocked window', function() {
+			var msg;
+
+			try {
+				getRouter().getFragment();
+			} catch (e) {
+				msg = e.message;
+			}
+
+			assert.equal('Need to mock window for node', msg);
+		});
+
+		it('should extract fragment from URL', function() {
+			var router = new Router();
+			router.window = getWindow();
+
+			assert('/things/0/stuff/0', router.getFragment());
+		});
+
+		it('should remove base URL from fragmnt', function() {
+			var router = new Router('/things/0');
+			router.window = getWindow();
+
+			assert('/stuff/0', router.getFragment());
+		});
+	});
+
+	describe('#on()', function() {
+
+		it('should work with a url and callback', function() {
+			var val = 0;
+
+			var router = getRouter()
+				.on('/a', function() {
+					val += 1;
+				})
+				.on('/b', function() {
+					val += 2;
+				})
+				.on('/c', function() {
+					val -= 1;
+				})
+
+			
+			router.run('/a');
+
+			assert.equal(val, 1);
+
+			router.run('/b');
+
+			assert.equal(val, 3);
+
+			router.run('/c');
+
+			assert.equal(val, 2);
+		});
+
+		it('should not failing when running routes with no callbacks', function() {
+			getRouter().run('/a');
+		});
+
+		it('should work with multiple URLs and a callback', function() {
+
+			var val = 0;
+
+			getRouter()
+				.on(['/a', '/b'], function() {
+					val += 1;
+				})
+			.run('/a').run('/b');
+
+			assert.equal(val, 2);
+		});
+
+		it('should work with one URL and multiple callbacks', function() {
+			var val = 0;
+
+			getRouter()
+				.on('/a', [
+					function() { val += 1; },
+					function() { val += 2; }
+				])
+			.run('/a');
+
+			assert.equal(val, 3);
+		});
+
+		it('should work with multiple URLs and multiple callbacks', function() {
+			var val = 0;
+
+			getRouter()
+				.on(['/a', '/b'], [
+					function() { val += 2; },
+					function() { val += 1; }
+				])
+			.run('/a').run('/b');
+
+			assert.equal(val, 6);
+		});
+	});
+
+	describe('#callFunctions()', function() {
+		it('should pass all arguments to called functions', function() {
+			var i = 0;
+
+			var fns = [function(j) {
+				i += j;
+			}, function(k) {
+				i += k - 1;
+			}];
+
+			var router = new Router();
+			var cb     = router.callFunctions(fns);
+
+			cb(3);
+
+			assert(5, i);
+		});
+	});
+
+	describe('#fallback()', function() {
+		it('should call a function when no routes are found', function() {
+
+			var val = 0;
+
+			getRouter().fallback(function() {
+				val += 2;
+			}).run('/a');
+
+			assert.equal(2, val);
+		});
+
+		it('should work with multiple fallback urls', function() {
+
+			var val = 0;
+
+			getRouter().fallback([function() {
+				val += 2;
+			}, function() {
+				val += 3;
+			}]).run('/a');
+
+			assert.equal(5, val);
+		});
+
+		it(
+			'should work with multiple fallback urls as multiple calls',
+			function() {
+
+				var val = 0;
+
+				getRouter().fallback(function() {
+					val += 2;
+				}).fallback(function() {
+					val += 3;
+				}).run('/a');
+
+				assert.equal(5, val);
+			}
+		);
+
+		it('should work when there are assigned routes', function() {
+			var val = 0;
+
+			getRouter()
+				.fallback(function() { val += 2; })
+				.on('/a', function() { val -= 1; })
+			.run('/b');
+
+			assert.equal(2, val);
+		});
+
+		it('should not call the fallback if a route is matched', function() {
+			var val = 0;
+
+			getRouter()
+				.fallback(function() { val += 2; })
+				.on('/a', function() { val -= 1; })
+			.run('/a');
+
+			assert.equal(-1, val);
+		});
+	});
+
+	describe('#controllerHandler()', function() {
+		it('should call a method on an object', function(done) {
+
+			var val = 0;
+
+			var router = getRouterWithRequire({
+				Foo: {
+					init: function() {
+						val = val + 1;
+					}
+				}
+			});
+
+			var fn = router.controllerHandler({
+				controller : 'Foo',
+				action     : 'init'
+			});
+
+			fn();
+
+			router.messages.once('controller', function() {
+				assert.equal(1, val);
+				done();
+			});
+		});
+
+		it('should pass the old fragment and new frag along', function(done) {
+			var val = 0;
+			var frag;
+			var cFrag;
+
+			var router = getRouterWithRequire({
+				Foo: {
+					init: function(oldFrag, currentFrag) {
+						cFrag = currentFrag;
+						val   = val + 1;
+					},
+
+					second: function(oldFrag) {
+						frag = oldFrag;
+					}
+				}
+			});
+
+			var fn = router.controllerHandler({
+				controller : 'Foo',
+				action     : 'init'
+			});
+
+			var fn2 = router.controllerHandler({
+				controller : 'Foo',
+				action     : 'second'
+			})
+
+			router.messages.once('controller', function() {
+
+				assert.equal(1, val);
+
+				router.messages.once('controller', function() {
+					assert.equal('/things/0/stuff/0', frag);
+					assert.equal('/things/0/stuff/0', cFrag);
+					done();
+				});
+
+
+				fn2();
+			});
+
+
+			fn();
+		});
+
+		it('should unload old controllers', function(done) {
+			var val = 0;
+
+			var router = getRouterWithRequire({
+				Foo: {
+					init: function(oldFrag, currentFrag) {
+						val = val + 1;
+					},
+
+					second: function(oldFrag) {
+						
+					},
+
+					unload: function() {
+						val = val - 1;
+					}
+				}
+			});
+
+			var fn = router.controllerHandler({
+				controller : 'Foo',
+				action     : 'init'
+			});
+
+			var fn2 = router.controllerHandler({
+				controller : 'Foo',
+				action     : 'second'
+			})
+
+			router.messages.once('controller', function() {
+
+				assert.equal(1, val);
+
+				router.messages.once('controller', function() {
+					assert.equal(0, val);
+					done();
+				});
+
+
+				fn2();
+			});
+
+			fn();
+		});
+	});
+
+	describe('#controller()', function() {
+		it('should work with a URL and controller name', function(done) {
+
+			var val = 0;
+
+			var router = getRouterWithRequire({
+				Foo: {
+					init: function() {
+						val = val + 1;
+					}
+				}
+			});
+			
+			router.controller('/a', 'Foo');			
+			router.messages.once('controller', function() {
+				assert.equal(1, val);
+
+				done();
+			});
+
+			router.run('/a');
+		});
+
+		it(
+			'should work with a URL and controller+action name',
+			function(done) {
+
+				var val = 0;
+
+				var router = getRouterWithRequire({
+					Foo: {
+						bar: function() {
+							val = val + 1;
+						}
+					}
+				});
+				
+				router.controller('/a', 'Foo', 'bar');			
+				router.messages.once('controller', function() {
+					assert.equal(1, val);
+
+					done();
+				});
+
+				router.run('/a');
+			}
+		);
+
+		it(
+			'should work with a URL config object',
+			function(done) {
+
+				var val = 0;
+
+				var router = getRouterWithRequire({
+					Foo: {
+						init: function() {
+							val = val + 1;
+						}
+					}
+				});
+				
+				router.controller('/a', { controller: 'Foo' });			
+				router.messages.once('controller', function() {
+					assert.equal(1, val);
+
+					done();
+				});
+
+				router.run('/a');
+			}
+		);
+
+		it(
+			'should work with a URL config object + action',
+			function(done) {
+
+				var val = 0;
+
+				var router = getRouterWithRequire({
+					Foo: {
+						bar: function() {
+							val = val + 1;
+						}
+					}
+				});
+				
+				router.controller('/a', { controller: 'Foo', action: 'bar' });			
+				router.messages.once('controller', function() {
+					assert.equal(1, val);
+
+					done();
+				});
+
+				router.run('/a');
+			}
+		);
+
+		it('should work with multiple URLs', function(done) {
+
+			var val = 0;
+
+			var router = getRouterWithRequire({
+				Foo: {
+					init: function() {
+						val = val + 1;
+					}
+				}
+			});
+			
+			router.controller(['/a', '/b'], 'Foo');			
+			router.messages.once('controller', function() {
+				assert.equal(1, val);
+
+				router.messages.once('controller', function() {
+					assert.equal(2, val);
+
+					done();
+				});
+
+				router.run('/b');
+			});
+
+			router.run('/a');
+		});
+	});
+	
+	describe('#fallback()', function() {
+		it('should work when there are no routes', function(done) {
+
+			var val = 0;
+
+			var router = getRouterWithRequire({
+				Foo: {
+					init: function() {
+						val = val + 1;
+					}
+				}
+			});
+			
+			router.fallbackController('Foo');			
+			router.messages.once('controller', function() {
+				assert.equal(1, val);
+
+				done();
+			});
+
+			router.run('/a');
+		});
+
+		it(
+			'should work when there are routes but route is not matched',
+			function(done) {
+
+				var val = 0;
+
+				var router = getRouterWithRequire({
+					Foo: {
+						init: function() {
+							val = val + 1;
+						}
+					}
+				});
+				
+				router.controller('/a', 'Foo');			
+				router.fallbackController('Foo');
+				router.messages.once('controller', function() {
+					assert.equal(1, val);
+
+					done();
+				});
+
+				router.run('/b');
+			}
+		);
+	});
+});
